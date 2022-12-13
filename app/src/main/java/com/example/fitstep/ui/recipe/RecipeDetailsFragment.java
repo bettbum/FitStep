1b4dbbd6e1308aa4d249f43425b511da3065257d
hong <bettbum@gmail.om>
2022-12-13 13:08:51 -0500

    13/12

--- app/src/main/java/com/example/fitstep/ui/recipe/RecipeDetailsFragment.java
+++ app/src/main/java/com/example/fitstep/ui/recipe/RecipeDetailsFragment.java
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.fitstep.R;
+import com.example.fitstep.models.GlobalData;
+import com.example.fitstep.models.Recipe;
+import com.squareup.picasso.Picasso;
 
 
 public class RecipeDetailsFragment extends Fragment {
@@ -18,8 +24,25 @@ public class RecipeDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_recipe_details, container, false);
-        RecipeDetailsFragment fragment = new RecipeDetailsFragment();
 
+        EditText edLabel = view.findViewById(R.id.edLabel);
+        EditText edCuisine = view.findViewById(R.id.edCuisine);
+        EditText edDescription = view.findViewById(R.id.edDesciption);
+        EditText edCalories = view.findViewById(R.id.edCalories);
+        ImageView imgFood = view.findViewById(R.id.imgFood);
+        TextView tvIngredients = view.findViewById(R.id.tvIngredientsFilled);
+
+        Recipe recipe = (Recipe) getArguments().getSerializable("recipe");
+        edLabel.setText(recipe.getLabel());
+        edCuisine.setText(recipe.getCuisineTypes());
+        edDescription.setText(recipe.getDescription());
+        edCalories.setText(recipe.getCalories());
+        Picasso.with(view.getContext()).load(recipe.getImage()).into(imgFood);
+        String ingredients = "";
+        for(String ingredient : recipe.getIngredients()){
+            ingredients = ingredients + ingredient + "\n";
+        }
+        tvIngredients.setText(ingredients);
 
         return view;
     }
